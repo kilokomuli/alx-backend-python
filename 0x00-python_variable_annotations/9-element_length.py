2cce012e59ff38ddcbd11d5f58e4bcfab23e3921
kilokomuli <kilokoemma175@gmail.com>
2024-07-05 07:47:18 -0700

    duct type and iterable object

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Duck type an iterable object"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples"""
+    return [(i, len(i)) for i in lst]
