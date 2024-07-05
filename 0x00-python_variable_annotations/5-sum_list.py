99619342a9481380e9f5552b3e05d2fb186cd434
kilokomuli <kilokoemma175@gmail.com>
2024-07-05 07:10:05 -0700

    fix linter

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Type-annotated function sum_list which takes
+a list input_list of floats as argument and returns their sum as a float.
+"""
 from typing import List
-"""Type-annotated function sum_list which takes a list input_list of floats as argument and returns their sum as a float."""
 
 
 def sum_list(input_list: List[float]) -> float:
