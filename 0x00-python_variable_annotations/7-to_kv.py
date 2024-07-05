4bb03edcdff69d35d02ea42c99e25e62edff1001
kilokomuli <kilokoemma175@gmail.com>
2024-07-05 07:24:29 -0700

    correction task7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-from typing import Union
+"""Typed annotation for complex types- string and  int/float to tuple
+"""
+from typing import List, Union, Tuple
 
-def to_kv(k: str, v: Union[int, float])-> tuple:
+
+def to_kv(k: str, v: Union[int, float])-> Tuple[str, float]:
     """Returns a tuple"""
-    return (k, v * v)
+    return (k, pow(v, 2))
