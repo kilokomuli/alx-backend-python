41a222f2dc429ea816ed0f1b4a53728c8814c6c8
kilokomuli <kilokoemma175@gmail.com>
2024-07-05 07:26:48 -0700

    fix linter

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float])-> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple"""
     return (k, pow(v, 2))
