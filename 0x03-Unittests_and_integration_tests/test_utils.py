a2bb4ec3d8ee590a5f1adff76f0d67d6adca7ad0
kilokomuli <kilokoemma175@gmail.com>
2024-07-29 12:03:03 +0300

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,13 @@
 """Parameterize a unit test"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+    )
+from unittest.mock import patch
+
 
 class TestAccessNestedMap(unittest.TestAccessNestedMap):
     """Defines class that inherits from unittest.TestCase."""
@@ -20,12 +26,52 @@ class TestAccessNestedMap(unittest.TestAccessNestedMap):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-    
+
     def test_access_nested_map_exception(self, nested_map, path):
         """Test access_nested_map function raises KeyError."""
-        with self.assertRaises(KeyError) as cm:
+        with self.assertRaises(KeyError) as err:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), repr(path[-1]))
+        self.assertEqual(str(err.exception), repr(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """Class module for unittest of get_json function."""
+    @patch('utils.requests.get')
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+
+    def test_get_json(self, test_url, test_payload):
+        """Tests if it returns the expected output."""
+        response = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **response)
+        mock_get = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with()
+        patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """Class module for unittest of memoize function."""
+    def test_memoize(self):
+        """Method"""
+
+
+        class TestClass:
+            """Wrapper class"""
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
 
 if __name__ == "__main__":
     unittest.main()
