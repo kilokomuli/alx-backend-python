c0951135a13036ac16e6f452aff055ec3780dc2c
kilokomuli <kilokoemma175@gmail.com>
2024-07-29 02:18:30 -0700

    fix lintetr

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,7 +17,6 @@ class TestAccessNestedMap(unittest.TestAccessNestedMap):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map function."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
@@ -26,7 +25,6 @@ class TestAccessNestedMap(unittest.TestAccessNestedMap):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-
     def test_access_nested_map_exception(self, nested_map, path):
         """Test access_nested_map function raises KeyError."""
         with self.assertRaises(KeyError) as err:
@@ -41,7 +39,6 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-
     def test_get_json(self, test_url, test_payload):
         """Tests if it returns the expected output."""
         response = {'return_value.json.return_value': test_payload}
@@ -57,7 +54,6 @@ class TestMemoize(unittest.TestCase):
     def test_memoize(self):
         """Method"""
 
-
         class TestClass:
             """Wrapper class"""
             def a_method(self):
@@ -72,6 +68,7 @@ class TestMemoize(unittest.TestCase):
             test_class.a_property()
             test_class.a_property()
             mock.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
