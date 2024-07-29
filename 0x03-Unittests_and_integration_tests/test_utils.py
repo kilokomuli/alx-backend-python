c974fa3ecafdbdd0617243c789002e02e7ce9fe9
kilokomuli <kilokoemma175@gmail.com>
2024-07-29 11:18:50 +0300

    task1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,9 +11,21 @@ class TestAccessNestedMap(unittest.TestAccessNestedMap):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
+
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map function."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map function raises KeyError."""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), repr(path[-1]))
 
 if __name__ == "__main__":
     unittest.main()
